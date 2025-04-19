1df1f775477699d8001d99778fad9b5c1012a966
Virgile RIETSCH <virgileboris96@hotmail.fr>
2025-04-19 10:42:04 +0200

    add routing module

---
+++ routing/2-1-premiere-route-statique/app/routes/users.tsx
@@ -0,0 +1,19 @@
+export default function Users() {
+    return (
+        <div className='px-8 py-2'>
+            <h1 className='text-2xl font-bold'>Utilisateurs</h1>
+            <ul>
+
+                <li className='text-lg font-mono'>
+                    <a href='/'>Virgile</a>
+                </li>
+                <li className='text-lg font-mono'>
+                    <a href='/'>Robert</a>
+                </li>
+                <li className='text-lg font-mono'>
+                    <a href='/'>John</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
