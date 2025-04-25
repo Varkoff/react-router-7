c8143395153da97a249707a41479cb4f847a31cc
Virgile RIETSCH <virgileboris96@hotmail.fr>
2025-04-25 14:43:02 +0200

    update

--- routing/2-2-chargement-cote-serveur/app/routes/users.tsx
+++ routing/2-2-chargement-cote-serveur/app/routes/users.tsx
@@ -1,12 +1,12 @@
-import { useLoaderData } from "react-router";
-import { getUsers } from "~/users.server";
+import { useLoaderData } from 'react-router';
+import { getUsers } from '~/users.server';
 
 export async function loader() {
-    return { users: await getUsers() }
+    return { users: await getUsers() };
 }
 
 export default function Users() {
-    const { users } = useLoaderData<typeof loader>()
+    const { users } = useLoaderData<typeof loader>();
     return (
         <div className='px-8 py-2'>
             <h1 className='text-2xl font-bold'>Utilisateurs</h1>
